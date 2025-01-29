6b9b3cbe0bf5ef262f5e935e94afc0a84e0a8bea
Rabi-Jerome <rabijerome.p@gmail.com>
2025-01-29 14:51:37 +0530

    Data Feed
    Bank creation, Domestic sales, Export sales, Product category, product creation, ptn, stock inward, stock return

--- include/views/bank_creation.php
+++ include/views/bank_creation.php
@@ -22,6 +22,17 @@
                         </tr>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>State Bank of India</td>
+                            <td>10983149186</td>
+                            <td>The WaterBase Limited</td>
+                            <td>Rajaji Salai</td>
+                            <td>SBININBB105</td>
+                            <td>SBIN0004804</td>
+                            <td>0004804-9000009</td>
+                            <td><span class="icon-border_color"></span></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
@@ -39,13 +50,13 @@
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                         <div class="form-group">
                             <label for="bank_id">Bank ID</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="bank_id" id="bank_id" placeholder="Bank ID" tabindex="1" disabled>
+                            <input type="text" class="form-control" name="bank_id" id="bank_id" value="B-101" tabindex="1" disabled>
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                         <div class="form-group">
                             <label for="bank_name">Bank Name</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="bank_name" id="bank_name" placeholder="Enter Bank Name" tabindex="2">
+                            <input type="text" class="form-control" name="bank_name" id="bank_name" value="State Bank of India" tabindex="2">
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
@@ -53,6 +64,7 @@
                             <label for="branch">Branch</label><span class="text-danger">*</span>
                             <select type="text" class="form-control" name="branch" id="branch" tabindex="3">
                                 <option>Select Branch</option>
+                                <option selected>Rajaji Salai</option>
                             </select>
                         </div>
                     </div>
@@ -61,31 +73,32 @@
                             <label for="account_type">Account Type</label><span class="text-danger">*</span>
                             <select type="text" class="form-control" name="account_type" id="account_type" tabindex="4">
                                 <option>Select Account Type</option>
+                                <option>current</option>
                             </select>
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                         <div class="form-group">
                             <label for="account_No">Account No</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="account_No" id="account_No" placeholder="Enter Account No" tabindex="5">
+                            <input type="text" class="form-control" name="account_No" id="account_No" value="10983149186" tabindex="5">
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                         <div class="form-group">
                             <label for="ifsc_code">IFSC Code</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="ifsc_code" id="ifsc_code" placeholder="Enter IFSC Code" tabindex="6">
+                            <input type="text" class="form-control" name="ifsc_code" id="ifsc_code" value="SBIN0004804" tabindex="6">
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                         <div class="form-group">
                             <label for="ad_code">AD Code</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="ad_code" id="ad_code" placeholder="Enter AD Code" tabindex="7">
+                            <input type="text" class="form-control" name="ad_code" id="ad_code" value="0004804-9000009" tabindex="7">
                         </div>
                     </div>
                     <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                         <div class="form-group">
                             <label for="Swift_code">Swift Code</label><span class="text-danger">*</span>
-                            <input type="text" class="form-control" name="Swift_code" id="Swift_code" placeholder="Enter Swift Code" tabindex="8">
+                            <input type="text" class="form-control" name="Swift_code" id="Swift_code" value="SBININBB105" tabindex="8">
                         </div>
                     </div>
                 </div>
@@ -94,10 +107,10 @@
     </div>
     <div class="row" style="margin-top: 30px;">
         <div class="col-md-6">
-            <button type="button" class="btn btn-primary" id="submit_user_creation">Excel Upload</button>
+            <button type="button" class="btn btn-primary" id="submit_user_creation" tabindex="9">Excel Upload</button>
         </div>
         <div class="col-md-6 text-right">
-            <button type="button" class="btn btn-primary" id="submit_user_creation">Submit</button>
+            <button type="button" class="btn btn-primary" id="submit_user_creation" tabindex="10">Submit</button>
         </div>
     </div>
 </div>
--- include/views/domestic_sales.php
+++ include/views/domestic_sales.php
@@ -12,7 +12,7 @@
                         <th rowspan="2" style="vertical-align: middle;">S.NO</th>
                         <th rowspan="2" style="vertical-align: middle;">Invoice No</th>
                         <th rowspan="2" style="vertical-align: middle;">Invoice Date</th>
-                        <th colspan="10" style="vertical-align: middle;">Sales Order Info</th>
+                        <th colspan="11" style="vertical-align: middle;">Sales Order Info</th>
                         <th colspan="3" style="vertical-align: middle;">Stock Issue</th>
                         <th colspan="4" style="vertical-align: middle;">Transporter Details</th>
                         <th rowspan="2" style="vertical-align: middle;">Action</th>
@@ -25,8 +25,9 @@
                         <th style="vertical-align: middle;">Po Date</th>
                         <th style="vertical-align: middle;">Lot No</th>
                         <th style="vertical-align: middle;">Storage Location</th>
-                        <th style="vertical-align: middle;">Rack No</th>
-                        <th style="vertical-align: middle;">Compartment No</th>
+                        <th style="vertical-align: middle;">Rack</th>
+                        <th style="vertical-align: middle;">Compartment</th>
+                        <th style="vertical-align: middle;">Palette No</th>
                         <th style="vertical-align: middle;">Qty</th>
                         <th style="vertical-align: middle;">Issue Date</th>
                         <th style="vertical-align: middle;">Issue Qty</th>
@@ -38,6 +39,30 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>TWL/EX/114/2024-25</td>
+                        <td>29-01-2025</td>
+                        <td>114</td>
+                        <td>DMC001</td>
+                        <td>Tender Cuts</td>
+                        <td>PI114</td>
+                        <td>28-12-2024</td>
+                        <td>28534M04TW001</td>
+                        <td>SL-1</td>
+                        <td>G</td>
+                        <td>A</td>
+                        <td>D</td>
+                        <td>4230</td>
+                        <td>29-01-2025</td>
+                        <td>4230</td>
+                        <td>17:30:00</td>
+                        <td>Murugan</td>
+                        <td>Murugan</td>
+                        <td>TN 13 K 3321</td>
+                        <td>183212416317</td>
+                        <td><span class="icon-border_color"></span></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -52,13 +77,13 @@
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="invoice_no">Invoice No</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="invoice_no" id="invoice_no" placeholder="Invoice No" tabindex="1" readonly>
+                                <input type="text" class="form-control" name="invoice_no" id="invoice_no" value="TWL/EX/114/2024-25" tabindex="1" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="invoice_date">Invoice Date</label><span class="text-danger">*</span>
-                                <input type="date" class="form-control" name="invoice_date" id="invoice_date" tabindex="2">
+                                <input type="date" class="form-control" name="invoice_date" id="invoice_date" value ="2025-01-29" tabindex="2">
                             </div>
                         </div>
                     </div>
@@ -77,63 +102,71 @@
                                 <label for="pi_no">PI No</label><span class="text-danger">*</span>
                                 <select type="text" class="form-control" name="pi_no" id="pi_no" tabindex="3">
                                     <option>Select PI No</option>
+                                    <option selected>114</option>
                                 </select>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="consignee_code">Consignee Code</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="consignee_code" id="consignee_code" placeholder="Consignee Code" tabindex="4" readonly>
+                                <input type="text" class="form-control" name="consignee_code" id="consignee_code" value="DMC001" tabindex="4" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="consignee_name">Consignee Name</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="consignee_name" id="consignee_name" placeholder="Consignee Name" tabindex="5" readonly>
+                                <input type="text" class="form-control" name="consignee_name" id="consignee_name" value="Tender Cuts" tabindex="5" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="po_no">Po No</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="po_no" id="po_no" placeholder="Po No" tabindex="6" readonly>
+                                <input type="text" class="form-control" name="po_no" id="po_no" value="PI114" tabindex="6" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="po_date">Po Date</label><span class="text-danger">*</span>
-                                <input type="date" class="form-control" name="po_date" id="po_date" placeholder="Po Date" tabindex="7" readonly>
+                                <input type="date" class="form-control" name="po_date" id="po_date" value="2024-12-28" tabindex="7" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="lot_no">Lot No</label><span class="text-danger">*</span>
-                                <select type="text" class="form-control" name="lot_no" id="lot_no" placeholder="Lot No" tabindex="8">
+                                <select type="text" class="form-control" name="lot_no" id="lot_no" tabindex="8">
                                     <option>Select Lot No</option>
+                                    <option value="28534M04TW001" selected>28534M04TW001</option>
                                 </select>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="storage_location">Storage Location</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="storage_location" id="storage_location" placeholder="Storage Location" tabindex="9" readonly>
+                                <input type="text" class="form-control" name="storage_location" id="storage_location" value="SL-1" tabindex="9" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
-                                <label for="rack_no">Rack No</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="rack_no" id="rack_no" placeholder="Rack No" tabindex="10" readonly>
+                                <label for="rack_no">Rack</label><span class="text-danger">*</span>
+                                <input type="text" class="form-control" name="rack_no" id="rack_no" value="G" tabindex="10" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
-                                <label for="compartment_no">Compartment No</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="compartment_no" id="compartment_no" placeholder="Compartment No" tabindex="11" readonly>
+                                <label for="compartment_no">Compartment</label><span class="text-danger">*</span>
+                                <input type="text" class="form-control" name="compartment_no" id="compartment_no" value="A" tabindex="11" readonly>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
+                            <div class="form-group">
+                                <label for="palette_no">Palette No</label><span class="text-danger">*</span>
+                                <input type="text" class="form-control" name="palette_no" id="palette_no" value="D" tabindex="12" readonly>
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="qty">Qty</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="qty" id="qty" placeholder="Qty" tabindex="12" readonly>
+                                <input type="text" class="form-control" name="qty" id="qty" value="4230" tabindex="13" readonly>
                             </div>
                         </div>
                     </div>
@@ -150,13 +183,13 @@
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="issue_date">Issue Date</label><span class="text-danger">*</span>
-                                <input type="date" class="form-control" name="issue_date" id="issue_date" placeholder="Enter Issue Date" tabindex="13">
+                                <input type="date" class="form-control" name="issue_date" id="issue_date" value="2025-01-29" tabindex="14">
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="issue_qty">Issue Qty</label><span class="text-danger">*</span>
-                                <input type="number" class="form-control" name="issue_qty" id="issue_qty" placeholder="Enter Issue Qty" tabindex="14">
+                                <input type="number" class="form-control" name="issue_qty" id="issue_qty" value="4230" tabindex="15">
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
@@ -164,15 +197,18 @@
                                 <label for="hours">Issue Time</label><span class="text-danger">*</span>
                                 <div class="form-row">
                                     <div class="col">
-                                        <input type="number" class="form-control" id="hours" name="hours" min="1" max="12" placeholder="HH" tabindex="15" readonly>
+                                        <input type="number" class="form-control" id="hours" name="hours" min="1" max="12" placeholder="HH" value="17" tabindex="16" readonly>
                                     </div>
                                     <div class="col">
-                                        <input type="number" class="form-control" id="minutes" name="minutes" min="0" max="59" placeholder="MM" tabindex="16" readonly>
+                                        <input type="number" class="form-control" id="minutes" name="minutes" min="0" max="59" placeholder="MM" value="30" tabindex="17" readonly>
                                     </div>
                                     <div class="col">
-                                        <select class="form-control" id="ampm" name="ampm" tabindex="17" style="height:34.6px; width: auto;" disabled>
+                                        <input type="number" class="form-control" id="seconds" name="seconds" min="0" max="59" placeholder="MM" value="00" tabindex="18" readonly>
+                                    </div>
+                                    <div class="col">
+                                        <select class="form-control" id="ampm" name="ampm" tabindex="19" style="height:34.6px; width: auto;" disabled>
                                             <option value="AM">AM</option>
-                                            <option value="PM">PM</option>
+                                            <option value="PM" selected>PM</option>
                                         </select>
                                     </div>
                                 </div>
@@ -192,25 +228,25 @@
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="transporter_name">Transporter Name</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="transporter_name" id="transporter_name" placeholder="Enter Transporter Name" tabindex="18">
+                                <input type="text" class="form-control" name="transporter_name" id="transporter_name" placeholder="Enter Transporter Name" value="Murugan" tabindex="20">
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="driver_name">Driver Name</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="driver_name" id="driver_name" placeholder="Enter Driver Name" tabindex="19">
+                                <input type="text" class="form-control" name="driver_name" id="driver_name" placeholder="Enter Driver Name" value="Murugan"tabindex="21">
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="vehicle_no">Vehicle No</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="vehicle_no" id="vehicle_no" placeholder="Enter Vehicle No" tabindex="20">
+                                <input type="text" class="form-control" name="vehicle_no" id="vehicle_no" placeholder="Enter Vehicle No" value="TN 13 K 3321" tabindex="22">
                             </div>
                         </div>
                         <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-4">
                             <div class="form-group">
                                 <label for="bill_no">E-Way Bill No</label><span class="text-danger">*</span>
-                                <input type="text" class="form-control" name="bill_no" id="bill_no" placeholder="Enter E-Way Bill No" tabindex="21">
+                                <input type="text" class="form-control" name="bill_no" id="bill_no" placeholder="Enter E-Way Bill No" value="183212416217" tabindex="23">
                             </div>
                         </div>
                     </div>
@@ -218,8 +254,8 @@
             </div>
         </div>
         <div class="text-right">
-            <button type="button" class="btn btn-primary" id="print_domestic_sales">Print</button>
-            <button type="button" class="btn btn-primary" id="submit_domestic_sales">Submit</button>
+            <button type="button" class="btn btn-primary" id="print_domestic_sales" tabindex="24">Print</button>
+            <button type="button" class="btn btn-primary" id="submit_domestic_sales" tabindex="25">Submit</button>
         </div>
     </div>
 </form>
